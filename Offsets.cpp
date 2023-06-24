e199f49cea7f3d27558b435cd0dd1e2037128ea9
ftyhfgh123 <132022425+ftyhfgh123@users.noreply.github.com>
2023-06-23 23:28:06 -0300

    Update Offsets.cpp

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,33 +1,32 @@
-//season 17 10/05/23
-//Riven
-//https://pastebin.com/SjMt0BdT
-#pragma once
-namespace offsets
-{
-// buttons
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const long IN_ATTACK = 0x074034b0; // [Buttons] -> in_attack
-// core
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const long REGION = 0x140000000; // [Mine] -> Region
-const long LEVEL = 0x165bd30; // [Miscellaneous] -> LevelName
-const long LOCAL_PLAYER = (0x02139480 + 0x8); // [Miscellaneous] -> LocalPlayer AVC_GameMovement + 0x8
-const long ENTITY_LIST = 0x1d87e88; // [Miscellaneous] -> cl_entitylist
-// entity
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel] -> m_localOrigin
-// player
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const long GLOW_ENABLE = (0x03c0 + 0x8); // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
-const long GLOW_THROUGH_WALL = (0x03c0 + 0x10); // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x10
-//const long GLOW_COLOR = 0x1d0; // [Miscellaneous] -> glow_color (use only if updated from main repo)
-const long TEAM_NUMBER = 0x044c; // [RecvTable.DT_BaseEntity] -> m_iTeamNum
-const long NAME = 0x0589; // [RecvTable.DT_BaseEntity] -> m_iName
-const long LIFE_STATE = 0x0798; // [RecvTable.DT_Player] -> m_lifeState
-const long VEC_PUNCH_WEAPON_ANGLE = 0x24c0; // [DataMap.C_Player] -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-const long VIEW_ANGLE = (0x25bc - 0x14); // [DataMap.C_Player] -> m_ammoPoolCapacity - 0x14
-const long BLEEDOUT_STATE = 0x2750; // [RecvTable.DT_Player] -> m_bleedoutState
-const long ZOOMING = 0x1c61; // [RecvTable.DT_Player] -> m_bZooming
-const long LAST_VISIBLE_TIME = 0x1A80; // [Miscellaneous] -> CPlayer!lastVisibleTime 1st offset from dump
-//const long CURRENT_SHIELDS = 0x0170; // m_shieldHealth (use only if updated from main repo)
-}
+    // Updated: 2023-06-20 // GameVersion v3.0.35.21
+     
+    #pragma once
+    namespace offsets
+    {
+        // buttons
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        const long IN_ATTACK = (0x07404610 + 0x8);            // [Buttons] -> in_attack+0x8
+        // core
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        const long REGION = 0x140000000;              // [Mine]          -> Region
+        const long LEVEL = 0x16fa230;                 // [Miscellaneous] -> LevelName
+        const long LOCAL_PLAYER = (0x22036C0 + 0x8);       // [Miscellaneous] -> AVC_GameMovement+0x8
+        const long ENTITY_LIST = 0x1e53c68;           // [Miscellaneous] -> cl_entitylist
+        // entity
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
+        // player
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        const long GLOW_ENABLE = (0x03c0 + 0x8);        // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
+        const long GLOW_THROUGH_WALL = (0x03c0 + 0x10); // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x10
+        const long GLOW_COLOR = 0x1D0;                  // [Miscellaneous]                  -> glow_color
+        const long TEAM_NUMBER = 0x044c;                // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
+        const long NAME = 0x0589;                       // [RecvTable.DT_BaseEntity]        -> m_iName
+        const long LIFE_STATE = 0x0798;                 // [RecvTable.DT_Player]            -> m_lifeState
+        const long VEC_PUNCH_WEAPON_ANGLE = 0x24b8;     // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+        const long VIEW_ANGLE = (0x25b4 - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
+        const long BLEEDOUT_STATE = 0x2750;             // [RecvTable.DT_Player]            -> m_bleedoutState
+        const long ZOOMING = 0x1c51;                    // [RecvTable.DT_Player]            -> m_bZooming
+        const long LAST_VISIBLE_TIME = 0x1a70;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
+        const long CURRENT_SHIELDS = 0x0170;            // [RecvTable.DT_BaseEntity]        -> m_shieldHealth
+    }
