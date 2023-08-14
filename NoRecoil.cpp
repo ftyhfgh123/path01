c0c04cb730dad7d7f9afbcd9023623a0cd639ce7
ftyhfgh123 <132022425+ftyhfgh123@users.noreply.github.com>
2023-08-14 19:18:03 -0300

    Update NoRecoil.cpp

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -9,8 +9,8 @@
 class NoRecoil
 {
 private:
-    const double m_streangthPitch = 0.2;
-    const double m_streangthYaw = 0.2;
+    const double m_streangthPitch = 0.1;
+    const double m_streangthYaw = 0.1;
     double m_previousPunchPitch = 0;
     double m_previousPunchYaw = 0;
 
