333f6ef9d9ef5cb06a45ac833d40c40bcec93da9
ftyhfgh123 <132022425+ftyhfgh123@users.noreply.github.com>
2023-08-16 17:26:54 -0300

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -10,7 +10,7 @@
 class Aimbot
 {
 private:
-    const int m_smoothing = 5;   // If you cross-hairs are not on target then this smoothness will be used.
+    const int m_smoothing = 3;   // If you cross-hairs are not on target then this smoothness will be used.
     const int m_activationFOV = 15; // FOV for activation
 
 public:
